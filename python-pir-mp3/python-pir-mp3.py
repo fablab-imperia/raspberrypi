20025f17e00c48e3e3ee368ef7a0ad24c4e1bd9a
Fablab Imperia <fablabimperia@gmail.com>
2017-09-07 00:12:20 +0200

    Aggiunto file mp3 da riprodurre e file di creazione servizio

--- python-pir-mp3/python-pir-mp3.py
+++ python-pir-mp3/python-pir-mp3.py
@@ -7,12 +7,14 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(5, GPIO.IN)
 GPIO.setup(6, GPIO.OUT)
 pygame.mixer.init()
-pygame.mixer.music.load("mare.mp3")
+pygame.mixer.music.load("/home/pi/Documents/python-pir-mp3/cascata.mp3")
 while True:
     x=GPIO.input(5)
-    print(x)
+    
     if x!=0: 
         GPIO.output(6, 1)
         if pygame.mixer.music.get_busy() != True :
             pygame.mixer.music.play()
-        sleep(0.1)
+    else: 
+		GPIO.output(6, 0)
+    sleep(0.1)
