0f4eab2afab423d21771048050c6609de0c0bae8
Stefano Semeria <stefano.semeria@gmail.com>
2018-03-18 14:49:18 +0100

    Nuova versione agganciata a iot.eclipse.org senza credenziali per  aggiornamento plugin

--- fablab-announcer/fablab_announcer.py
+++ fablab-announcer/fablab_announcer.py
@@ -1,12 +1,28 @@
 import paho.mqtt.client as mqtt
 import time
 
+UPDATE_TIME_IN_SECONDS = 60
+
+
+
+
 client = mqtt.Client()
-client.will_set(topic='fablab/status',payload='closed')
-client.username_pw_set('MQTT_USERNAME','MQTT_PASSWORD')
-client.connect('m23.cloudmqtt.com',19543)
+client.will_set(topic='fablab/status',payload='chiuso', retain=True)
+client.connect('iot.eclipse.org',1883)
+
+#Say to world that fablab is open
+client.publish(topic='fablab/status',payload='aperto' ,retain=True)
+
+#Update time
+now = time.time()
+last_time_we_update = now
 
 
 while True:
-    client.publish(topic='fablab/status',payload='open',retain=True)
-    time.sleep(60)
+    now = time.time()
+    if (now - last_time_we_update >= UPDATE_TIME_IN_SECONDS) :
+        client.publish(topic='fablab/status',payload='aperto',retain=True)
+        last_time_we_update = time.time()
+    client.loop(1)
+    #This just to slow down updates with ping
+    time.sleep(5)
